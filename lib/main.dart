c8e02d0617bcf4af4f86950e40827aae80cca1aa
Stanisław Szewerda <stanislaw.szewerda@gmail.com>
2022-08-21 18:33:43 +0200

    Add SecondPage

--- lib/main.dart
+++ lib/main.dart
@@ -62,8 +62,8 @@ class WelcomePage extends StatelessWidget {
             ElevatedButton(
               style: ElevatedButton.styleFrom(primary: Colors.black),
               onPressed: () {
-                //Navigator.of(context).push(
-                //MaterialPageRoute(builder: (_) => const SecondPage()));
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => const SecondPage()));
               },
               child: Text(
                 'Rozpocznij',
@@ -82,3 +82,152 @@ class WelcomePage extends StatelessWidget {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class SecondPage extends StatefulWidget {
+  const SecondPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+final emailcontroler = TextEditingController();
+
+final passwordcontroler = TextEditingController();
+
+class _SecondPageState extends State<SecondPage> {
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/window.jpg'), fit: BoxFit.cover),
+        ),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Smart Home',
+              style: GoogleFonts.lobster(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                textStyle:
+                    const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+            ),
+            Text(
+              'Just for You',
+              style: GoogleFonts.lobster(
+                fontSize: 30,
+                textStyle:
+                    const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(20.0),
+              decoration:
+                  const BoxDecoration(color: Color.fromARGB(99, 0, 0, 0)),
+              child: Center(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        focusColor: const Color.fromARGB(255, 255, 255, 255),
+                        border: const OutlineInputBorder(),
+                        label: const Center(child: Text('E-mail')),
+                        labelStyle: TextStyle(
+                            color: myFocusNode.hasFocus
+                                ? const Color.fromARGB(255, 255, 255, 255)
+                                : const Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                      controller: emailcontroler,
+                      style: GoogleFonts.lobster(
+                          fontSize: 20,
+                          textStyle: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255))),
+                    ),
+                    const SizedBox(
+                      height: 1,
+                    ),
+                    TextFormField(
+                      focusNode: myFocusNode,
+                      decoration: InputDecoration(
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        focusColor: const Color.fromARGB(255, 255, 255, 255),
+                        border: const OutlineInputBorder(),
+                        label: const Center(child: Text('Password')),
+                        labelStyle: TextStyle(
+                            color: myFocusNode.hasFocus
+                                ? const Color.fromARGB(255, 255, 255, 255)
+                                : const Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                      controller: passwordcontroler,
+                      style: GoogleFonts.lobster(
+                          fontSize: 20,
+                          textStyle: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255))),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.blue),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        'Zaloguj',
+                        style: GoogleFonts.lobster(
+                          fontSize: 30,
+                          textStyle: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Zarejestruj się',
+                        style: GoogleFonts.lobster(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          textStyle: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Colors.blue),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Ekran startowy',
+                style: GoogleFonts.lobster(
+                  fontSize: 30,
+                  textStyle: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
