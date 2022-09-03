d4d87669f9821f087281fb7149523c3fd49ba657
Stanisław Szewerda <stanislaw.szewerda@gmail.com>
2022-09-03 15:21:44 +0200

    Add cloud_firestore to iot_devices_cubit

--- lib/app/home/iot_devices/cubit/iot_devices_cubit.dart
+++ lib/app/home/iot_devices/cubit/iot_devices_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
@@ -9,14 +11,37 @@ class IotDevicesCubit extends Cubit<IotDevicesState> {
       : super(const IotDevicesState(
             documents: [], errorMessage: '', isLoading: false));
 
+  StreamSubscription? _streamSubscription;
+
   Future<void> start() async {
+    //Loading
     emit(
       const IotDevicesState(documents: [], errorMessage: '', isLoading: true),
     );
-    await Future.delayed(const Duration(seconds: 5));
-    emit(
-      const IotDevicesState(
-          documents: [], errorMessage: 'Błąd połącznia', isLoading: false),
-    );
+
+    _streamSubscription = FirebaseFirestore.instance
+        .collection('devices')
+        .orderBy('portnumber')
+        .snapshots()
+        .listen((data) {
+      emit(
+        IotDevicesState(
+            documents: data.docs, errorMessage: '', isLoading: false),
+      );
+    })
+      ..onError((error) {
+        emit(
+          IotDevicesState(
+              documents: const [],
+              errorMessage: error.toString(),
+              isLoading: true),
+        );
+      });
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
   }
 }
--- lib/app/home/iot_devices/home_page_iot_devices_content.dart
+++ lib/app/home/iot_devices/home_page_iot_devices_content.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_home/app/home/iot_devices/cubit/iot_devices_cubit.dart';
@@ -60,58 +59,5 @@ class IotDevices extends StatelessWidget {
             );
           },
         )));
-
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      stream: FirebaseFirestore.instance
-          .collection('devices')
-          .orderBy('portnumber')
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Text('something went wrong');
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text('Loading');
-        }
-
-        final documents = snapshot.data!.docs;
-
-        return ListView(
-          children: [
-            Container(
-              decoration:
-                  const BoxDecoration(color: Color.fromARGB(255, 152, 163, 0)),
-              padding: const EdgeInsets.all(20.0),
-              margin: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text('Device name'),
-                  Text('Port number'),
-                  Text('State'),
-                  Text('Button'),
-                ],
-              ),
-            ),
-            for (final document in documents) ...[
-              Container(
-                decoration:
-                    const BoxDecoration(color: Color.fromARGB(61, 85, 255, 7)),
-                padding: const EdgeInsets.all(20.0),
-                margin: const EdgeInsets.all(20.20),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(document['name']),
-                      Text(document['portnumber'].toString()),
-                      const Text('value'),
-                      const Text('button'),
-                    ]),
-              ),
-            ],
-          ],
-        );
-      },
-    );
   }
 }
