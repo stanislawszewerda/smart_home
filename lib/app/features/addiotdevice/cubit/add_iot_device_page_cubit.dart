48c91ff85497b7844ece9067be43ae776be80d42
Stanisław Szewerda <stanislaw.szewerda@gmail.com>
2022-09-07 21:05:07 +0200

    Create Cubit for AddIotDevicePage

---
+++ lib/app/features/addiotdevice/cubit/add_iot_device_page_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_iot_device_page_state.dart';
+
+class AddIotDevicePageCubit extends Cubit<AddIotDevicePageState> {
+  AddIotDevicePageCubit() : super(AddIotDevicePageState());
+}
---
+++ lib/app/features/addiotdevice/cubit/add_iot_device_page_state.dart
@@ -0,0 +1,4 @@
+part of 'add_iot_device_page_cubit.dart';
+
+@immutable
+class AddIotDevicePageState {}
