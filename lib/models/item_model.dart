867f7e827110084dcff5aac7b8479cd6718335ce
Stanisław Szewerda <stanislaw.szewerda@gmail.com>
2022-09-26 22:07:27 +0200

    Delete required from iotdevice state

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,6 +1,12 @@
 class ItemModel {
-  ItemModel(this.portNumber, this.iotDeviceName);
+  ItemModel(
+      {required this.portNumber,
+      required this.iotDeviceName,
+      required this.iotType,
+      required this.state});
 
-  final String portNumber;
+  final int portNumber;
   final String iotDeviceName;
+  final String iotType;
+  final bool state;
 }
